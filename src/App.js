6c23791c75cd89bac497a761e29872ece6e249f0
Pedro <tampocosequeponerenesteespacio@gmail.com>
2021-11-25 15:36:27 -0300

    Basic functions completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
+const App = () => {
+  const [currentOp, setCurrentOp] = useState('')
+  const [prevOp, setPrevOp] = useState('')
+  const [operator, setOperator] = useState('')
+
+const evaluate = () => {
+  const curr = parseFloat(currentOp)
+  const prev = parseFloat(prevOp)
+  switch (operator) {
+    case '+':
+      return (prev+curr).toString()
+    case '-':
+      return (prev-curr).toString()
+    case 'x':
+      return (prev*curr).toString()
+    case '÷':
+      return (prev/curr).toString()
+  }
+}
+
+const handleDigit = (e) => {
+  const digit = e.target.name
+  if (currentOp === '0') {
+    setCurrentOp(digit)
+    return
+  }
+  if (currentOp.includes('.') && digit === '.') return
+  setCurrentOp(currentOp+digit)
+  
+}
+
+const handleOperator = (e) => {
+  const op = e.target.name
+
+  if (currentOp === '.') return
+  
+  if (currentOp && prevOp && !operator ) {
+    setOperator(op)
+    setPrevOp(currentOp)
+    setCurrentOp('')
+    return
+  }
+  
+  if (!currentOp && operator ) {
+    setOperator(op)
+    return
+  }
+
+  if (currentOp  && prevOp ) {
+    setPrevOp(evaluate())
+    setCurrentOp('')
+    setOperator(op)
+    return
+  }
+
+  if (!currentOp && !operator ) {
+    setOperator(op)
+    return
+  }
+
+  setOperator(op)
+  setPrevOp(currentOp)
+  setCurrentOp('')  
+}
+
+const handleAC = () => {
+  setPrevOp('')
+  setCurrentOp('')
+  setOperator('')
+}
+
+const handleEqual = () => {
+  if (prevOp === '' || currentOp === '' || operator === '') return
+  if (currentOp === '.') return
+  setPrevOp(evaluate())
+  setCurrentOp('')
+  setOperator('')
+}
+
+const handleDel = () => {
+  if (currentOp) {
+    setCurrentOp(currentOp.slice(0,-1))
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main'>
+      
+      <div id='calculator'>
+        <div id='output'>
+        <div id='prev'>{prevOp} {operator}</div>
+        <div id='curr'>{currentOp}</div>
+        </div>
+        <button name='ac' className='span2' onClick={handleAC} >AC</button>
+        <button name='del' onClick={handleDel} >DEL</button>
+        <button name='÷' onClick={handleOperator} >÷</button>
+        <button name='7' onClick={handleDigit} >7</button>
+        <button name='8' onClick={handleDigit} >8</button>
+        <button name='9' onClick={handleDigit} >9</button>
+        <button name='x' onClick={handleOperator} >X</button>
+        <button name='4' onClick={handleDigit} >4</button>
+        <button name='5' onClick={handleDigit} >5</button>
+        <button name='6' onClick={handleDigit} >6</button>
+        <button name='-' onClick={handleOperator} >-</button>
+        <button name='1' onClick={handleDigit} >1</button>
+        <button name='2' onClick={handleDigit} >2</button>
+        <button name='3' onClick={handleDigit} >3</button>
+        <button name='+' onClick={handleOperator} >+</button>
+        <button name='.' onClick={handleDigit} >.</button>
+        <button name='0' onClick={handleDigit} >0</button>
+        <button name='?'  ></button>
+        <button name='=' onClick={handleEqual} >=</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
