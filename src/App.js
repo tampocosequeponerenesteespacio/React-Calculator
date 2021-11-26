85c265cc4bb8688c14f7b77b2d482a5fe02d83f4
Pedro <tampocosequeponerenesteespacio@gmail.com>
2021-11-26 09:29:58 -0300

    added style, color and footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import {FiDelete} from 'react-icons/fi'
+
 
 const App = () => {
   const [currentOp, setCurrentOp] = useState('')
@@ -19,6 +21,7 @@ const evaluate = () => {
       return (prev/curr).toString()
   }
 }
+
 
 const handleDigit = (e) => {
   const digit = e.target.name
@@ -85,34 +88,38 @@ const handleDel = () => {
 }
 
   return (
-    <div id='main'>
-      
-      <div id='calculator'>
-        <div id='output'>
-        <div id='prev'>{prevOp} {operator}</div>
-        <div id='curr'>{currentOp}</div>
+    <>
+      <div id='main'>
+        
+        <div id='calculator'>
+          <div id='output'>
+            <div id='prev'>{prevOp} {operator}</div>
+            <div id='curr'>{currentOp}</div>
+          </div>
+          <button name='ac' className='span2' onClick={handleAC} >AC</button>
+          <button name='del' onClick={handleDel} ><FiDelete size='1.5em' /></button>
+          <button name='÷' onClick={handleOperator} >÷</button>
+          <button name='7' onClick={handleDigit} >7</button>
+          <button name='8' onClick={handleDigit} >8</button>
+          <button name='9' onClick={handleDigit} >9</button>
+          <button name='x' onClick={handleOperator} >X</button>
+          <button name='4' onClick={handleDigit} >4</button>
+          <button name='5' onClick={handleDigit} >5</button>
+          <button name='6' onClick={handleDigit} >6</button>
+          <button name='-' onClick={handleOperator} >-</button>
+          <button name='1' onClick={handleDigit} >1</button>
+          <button name='2' onClick={handleDigit} >2</button>
+          <button name='3' onClick={handleDigit} >3</button>
+          <button name='+' onClick={handleOperator} >+</button>
+          <button name='.' onClick={handleDigit} >.</button>
+          <button name='0' onClick={handleDigit} >0</button>
+          <button name='=' className='span2' onClick={handleEqual} >=</button>
         </div>
-        <button name='ac' className='span2' onClick={handleAC} >AC</button>
-        <button name='del' onClick={handleDel} >DEL</button>
-        <button name='÷' onClick={handleOperator} >÷</button>
-        <button name='7' onClick={handleDigit} >7</button>
-        <button name='8' onClick={handleDigit} >8</button>
-        <button name='9' onClick={handleDigit} >9</button>
-        <button name='x' onClick={handleOperator} >X</button>
-        <button name='4' onClick={handleDigit} >4</button>
-        <button name='5' onClick={handleDigit} >5</button>
-        <button name='6' onClick={handleDigit} >6</button>
-        <button name='-' onClick={handleOperator} >-</button>
-        <button name='1' onClick={handleDigit} >1</button>
-        <button name='2' onClick={handleDigit} >2</button>
-        <button name='3' onClick={handleDigit} >3</button>
-        <button name='+' onClick={handleOperator} >+</button>
-        <button name='.' onClick={handleDigit} >.</button>
-        <button name='0' onClick={handleDigit} >0</button>
-        <button name='?'  ></button>
-        <button name='=' onClick={handleEqual} >=</button>
       </div>
-    </div>
+      <div id='footer'>
+      Copyright © 2021 Tampoco
+      </div>
+    </>
   )
 }
 
